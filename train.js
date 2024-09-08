a9d3ef63b09329eb2db53ed04449c7ca88d685d3
yousra <160774839+arsouuy@users.noreply.github.com>
2024-09-08 19:04:51 +0100

    Update train.js

--- train.js
+++ train.js
@@ -1,1 +1,18 @@
-console.log("hello")
+let reddiv = document.getElementById('red');
+reddiv.onclick=()=>console.log('you clicked on the red ');
+
+
+
+let yellowdiv = document.getElementById('yellow');
+yellowdiv.onclick=()=>console.log('you clicked on the yellow');
+
+
+let greendiv = document.getElementById('green');
+greendiv.onclick=()=>console.log('you clicked on the green');              
+
+
+const  buttons = document.querySelectorAll(".button") 
+ buttons.forEach(buttony => {
+buttony.onclick =()=>console.log(buttony.value)  
+ });
+ 
